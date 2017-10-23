d1e28216791b9ed15da0dd7eecdf286cf51c61f9
luoyao1 <luoyao1@yonyou.com>
2017-10-23 20:56:24 +0800

    坑爹

--- demo/src/main/java/yc/yonyou/hello/HelloController.java
+++ demo/src/main/java/yc/yonyou/hello/HelloController.java
@@ -2,18 +2,23 @@ package yc.yonyou.hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 	
 	private final static Logger LOG = LoggerFactory.getLogger(HelloController.class);
 	
+	@Value(value = "${from}")
+	private String from;
+	
 	@RequestMapping("/hello")
 	public String hello(){
+		LOG.info("from:【{}】",from);
 		return "hello world";
 	}
 }
